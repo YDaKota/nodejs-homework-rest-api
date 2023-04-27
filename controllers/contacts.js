1a9741e1cbe085c8420d548925138e73baee343c
YDaKota <95923839+YDaKota@users.noreply.github.com>
2023-04-27 19:18:52 +0300

    add main files

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,14 +1,14 @@
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-    const result = await contacts.getAll();
+    const result = await Contact.find({}, "-createdAt -updatedAt");
     res.json(result);
 }
 
 const getById = async (req, res) => {
-    const { id } = req.params;
-    const result = await contacts.getById(id);
+    const { contactId } = req.params;
+    const result = await Contact.findById(contactId);
     if (!result) {
         throw HttpError(404, "Not found");
     }
@@ -16,13 +16,13 @@ const getById = async (req, res) => {
 }
 
 const add = async (req, res) => {
-    const result = await contacts.add(req.body);
+    const result = await Contact.add(req.body);
     res.status(201).json(result);
 }
 
 const updateById = async (req, res) => {
-    const { id } = req.params;
-    const result = await contacts.updateById(id, req.body);
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
     if (!result) {
         throw HttpError(404, "Not found");
     }
@@ -30,8 +30,8 @@ const updateById = async (req, res) => {
 }
 
 const deleteById = async (req, res) => {
-    const { id } = req.params;
-    const result = await contacts.deleteById(id);
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndRemove(contactId);
     if (!result) {
         throw HttpError(404, "Not found");
     }
@@ -40,10 +40,26 @@ const deleteById = async (req, res) => {
     })
 }
 
+const updateFavorite = async (req, res) => {
+	const { contactId } = req.params;
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+	if (!result) {
+		res.status(400).json({ message: 'missing field favorite' })
+	}
+	const contact = await Contact.updateStatusContact(contactId)
+
+	if (contact) {
+		res.status(200).json(contact)
+	} else {
+		res.status(404).json({ message: 'Not found' })
+	}
+}
+
 module.exports = {
     getAll: ctrlWrapper(getAll),
     getById: ctrlWrapper(getById),
     add: ctrlWrapper(add),
     updateById: ctrlWrapper(updateById),
     deleteById: ctrlWrapper(deleteById),
+    updateFavorite: ctrlWrapper(updateFavorite),
 }
