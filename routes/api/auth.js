66da998bb5ef2ee4fded03b1dc9d4433697bdd51
YDaKota <95923839+YDaKota@users.noreply.github.com>
2023-05-03 22:47:01 +0300

    add main files

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,6 +6,8 @@ const ctrl = require("../../controllers/auth");
 const router = express.Router();
 
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.get("/verify/:verificationCode", ctrl.verifyEmail);
+router.post("/verify", validateBody(schemas.verifyEmailSchema), ctrl.resendVerifyEmail);
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
